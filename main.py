6710a501aff17e198d90c5f1dc2fd22259ac5119
sheelykyle <kyle@kylesheely.com>
2024-04-09 19:47:57 -0400

    Added function which counts character frequency in text

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
+letter_dictionary = {}
+
 def main():
     with open("books/frankenstein.txt") as book:
         text = book.read()
         number_of_words = countWords(text)
         print(number_of_words)
 
+        number_of_letters = countLetters(text)
+        print(letter_dictionary)
+
 def countWords(text):
     words = text.split()
     return len(words)
 
+def countLetters(text):
+    for word in text:
+        lowercase_letter = word.lower()
+        if lowercase_letter in letter_dictionary:
+            letter_dictionary[lowercase_letter] += 1
+        else:
+            letter_dictionary[lowercase_letter] = 1
+    return letter_dictionary
 main()
