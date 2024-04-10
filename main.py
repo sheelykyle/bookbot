cc82fbfde73700c6b61867f3df4d62c7b328a36d
sheelykyle <kyle@kylesheely.com>
2024-04-09 20:28:18 -0400

    Broke out functions, limited count and report to alphabetical characters

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ def main():
         text = book.read()
         number_of_words = countWords(text)
         number_of_letters = countLetters(text)
-    letter_dictionary = sorted(letter_dictionary, key=letter_dictionary.get, reverse=True)
+    sorted_dict = sortDict(letter_dictionary)
     print(f"--- Begin report of {book_path} ---")
     print(f"{number_of_words} words found in the document")
-    for character in letter_dictionary:
+    for character in sorted_dict:
         print(f"The {character} was found {letter_dictionary[character]} times")
     print("--- End report ---")
+
+def sortDict(letter_dictionary):
+    sorted_dict = sorted(letter_dictionary, key=letter_dictionary.get, reverse=True)
+    return sorted_dict
 
 def countWords(text):
     words = text.split()
@@ -20,10 +24,11 @@ def countWords(text):
 def countLetters(text):
     for word in text:
         lowercase_letter = word.lower()
-        if lowercase_letter in letter_dictionary:
-            letter_dictionary[lowercase_letter] += 1
-        else:
-            letter_dictionary[lowercase_letter] = 1
+        if lowercase_letter.isalpha() == True:
+            if lowercase_letter in letter_dictionary:
+                letter_dictionary[lowercase_letter] += 1
+            else:
+                letter_dictionary[lowercase_letter] = 1
     return letter_dictionary
 
 main()
